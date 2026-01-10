c470effea42cc9a9663ad79ea111986fa522eb44
Leonid Kostrykin <void@evoid.de>
2026-01-10 22:10:16 +0100

    Fix flake8 linting

--- scripts/fetch-tools.py
+++ scripts/fetch-tools.py
@@ -9,27 +9,40 @@ import git
 import yaml
 
 ts = bioblend.toolshed.ToolShedInstance(url='https://toolshed.g2.bx.psu.edu')
+default_repo_url = 'https://github.com/BMCV/galaxy-image-analysis.git'
 
 
 def list_tool_repositories(repo_url: str):
     with tempfile.TemporaryDirectory() as tempdir_str:
         tempdir = pathlib.Path(tempdir_str)
         git.Repo.clone_from(repo_url, tempdir_str)
-        for shed_yml_path in tempdir.glob('tools/**/.shed.y*ml', case_sensitive=False):
+        for shed_yml_path in tempdir.glob(
+            'tools/**/.shed.y*ml',
+            case_sensitive=False,
+        ):
             if re.match(r'^.*\.ya?ml$', str(shed_yml_path).lower()):
                 with shed_yml_path.open('r') as fp:
                     shed = yaml.safe_load(fp)
-                repo_name = shed.get('suite', dict()).get('name') or shed.get('name')
+                repo_name = (
+                    shed.get('suite', dict()).get('name')
+                    or shed.get('name')
+                )
                 repo_owner = shed.get('owner')
                 if repo_name and repo_owner:
                     if repo_name.startswith('suite_'):
-                        install_info = ts.repositories.get_repository_revision_install_info(
+                        install_info = (
+                            ts
+                            .repositories
+                            .get_repository_revision_install_info
+                        )(
                             repo_name,
                             repo_owner,
                             list_revisions(repo_name, repo_owner)[-1],
                         )
                         suite_structure = install_info[2][repo_name][5]
-                        for repo_info in suite_structure[suite_structure['root_key']]:
+                        for repo_info in suite_structure[
+                            suite_structure['root_key']
+                        ]:
                             yield repo_info[1], repo_owner
                     else:
                         yield repo_name, repo_owner
@@ -37,7 +50,10 @@ def list_tool_repositories(repo_url: str):
 
 def list_revisions(repo_name: str, repo_owner: str) -> list[str]:
     try:
-        return ts.repositories.get_ordered_installable_revisions(repo_name, repo_owner)
+        return ts.repositories.get_ordered_installable_revisions(
+            repo_name,
+            repo_owner,
+        )
     except Exception as e:
         print(e, file=sys.stderr)
         return list()
@@ -54,7 +70,9 @@ class SectionIndex:
         for section in sections:
             for tool in section.get('tools', list()):
                 if tool in self._data:
-                    raise ValueError(f'Tool "{tool}" defined in multple sections.')
+                    raise ValueError(
+                        f'Tool "{tool}" defined in multple sections.',
+                    )
                 self._data[tool] = section
 
     def __getitem__(self, repo_name: str) -> str:
@@ -85,7 +103,8 @@ def build_tools_dict(repo_url: str, verbose: bool = False) -> dict:
             )
             if verbose:
                 print(
-                    f'{tools[-1]["owner"]}/{tools[-1]["name"]}: {tools[-1]["revisions"]}',
+                    f'{tools[-1]["owner"]}/{tools[-1]["name"]}:',
+                    f'{tools[-1]["revisions"]}',
                     f'({tools[-1]["tool_panel_section_label"]})'
                 )
     if sections.unused:
@@ -101,15 +120,15 @@ def build_tools_dict(repo_url: str, verbose: bool = False) -> dict:
         tools=tools,
         install_resolver_dependencies=False,
 
-        # Keep the image small, don't install Conda dependencies, run the container with
-        # `--priviledged` to employ Singularity constainers instead:
+        # Keep the image small, skip Conda dependencies, run the container
+        # with `--priviledged` to employ Singularity constainers instead:
         install_tool_dependencies=False,
     )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--repo-url', type=str, default='https://github.com/BMCV/galaxy-image-analysis.git')
+    parser.add_argument('--repo-url', type=str, default=default_repo_url)
     parser.add_argument('tools_file', type=str)
     parser.add_argument('--verbose', action='store_true', default=False)
     args = parser.parse_args()
